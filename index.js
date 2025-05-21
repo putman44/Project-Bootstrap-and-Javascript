024f95bd9daa5748a82f97ec7b0f4d3bd4bb6d63
Taylor Putman <fishputman@gmail.com>
2025-05-21 15:18:22 -0400

    added products object

---
+++ index.js
@@ -0,0 +1,25 @@
+Products = {
+  campingTents: {
+    northFace: {
+      image:
+        "https://images.unsplash.com/photo-1496545672447-f699b503d270?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: "All-weather tent for a comfortable and dry adventure.",
+      price: "$120",
+      inStock: "Yes",
+    },
+    coleman: {
+      image:
+        "https://images.unsplash.com/photo-1565588496723-63494874b143?q=80&w=1732&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: "Spacious, easy-to-set-up tent for families and groups.",
+      price: "$110",
+      inStock: "Yes",
+    },
+    rei: {
+      image:
+        "https://images.unsplash.com/photo-1742138373997-a123057b834b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: "Lightweight, durable tent ideal for backpacking trips.",
+      price: "$140",
+      inStock: "Limited",
+    },
+  },
+};
